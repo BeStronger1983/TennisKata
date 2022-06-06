997cf4b31ba9b866cc3668885ab2a9a79f1ceb6b
ponpon <wizardsoloist1983@gmail.com>
2022-06-06 18:22:28 +0800

    https://www.youtube.com/watch?v=0910KxWDLzk&t=535s

---
+++ tennis.js
@@ -0,0 +1,26 @@
+export class Tennis {
+    firstPlayerScoreTimes = 0;
+
+    scoreLookup = {
+        0: 'love',
+        1: 'fifteen',
+        2: 'thirty',
+        3: 'forty',
+    };
+    secondPlayerScoreTimes = 0;
+
+    score() {
+        if (this.secondPlayerScoreTimes !== this.firstPlayerScoreTimes) {
+            return `${this.scoreLookup[this.firstPlayerScoreTimes]} ${this.scoreLookup[this.secondPlayerScoreTimes]}`
+        }
+        return `${this.scoreLookup[this.firstPlayerScoreTimes]} all`
+    }
+
+    firstPlayerScore() {
+        this.firstPlayerScoreTimes++;
+    }
+
+    secondPlayerScore() {
+        this.secondPlayerScoreTimes++;
+    }
+}
